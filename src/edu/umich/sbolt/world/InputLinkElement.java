e2bafcbe50b7de82e2484cdac5bb7c073bbc8a08
Aaron Mininger <mininger@umich.edu>
2011-12-07 12:59:24 -0500

    changed Object to WorldObject

--- src/edu/umich/sbolt/world/InputLinkElement.java
+++ src/edu/umich/sbolt/world/InputLinkElement.java
@@ -3,10 +3,18 @@ package edu.umich.sbolt.world;
 import sml.Identifier;
 import sml.WMElement;
 
+/**
+ * An interface for objects that will get written to the InputLink
+ * 
+ * @author mininger
+ * 
+ */
 public interface InputLinkElement
 {    
+    // update the input-link appropriately
     void updateInputLink(Identifier parentIdentifier);
     
+    // remove the object from the input-link
     void destroy();
    
 }
