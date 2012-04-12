f017955c508bbad25d7b41baa9a133167bca605b
Aaron Mininger <mininger@umich.edu>
2012-04-12 15:08:57 -0400

    small fix to SBolt

--- src/edu/umich/sbolt/SBolt.java
+++ src/edu/umich/sbolt/SBolt.java
@@ -115,7 +115,7 @@ public class SBolt implements LCMSubscriber
         // Otherwise the system would apparently hang on a commit
         kernel.SetAutoCommit(false);
 
-        System.out.println("Spawn Debugger: " + agent.SpawnDebugger(kernel.GetListenerPort(), "/home/aaron/soar/SoarSuite/out/SoarJavaDebugger.jar"));
+        System.out.println("Spawn Debugger: " + agent.SpawnDebugger(kernel.GetListenerPort()));
 
         world = new World();
 
