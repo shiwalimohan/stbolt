3493b01adbefef05dae1499d92403f25ccf5e334
Sam Wintermute <sam.wintermute@soartech.com>
2012-04-17 14:14:32 -0400

    debug tools and message structures for planned demo sentences

---
+++ lgsoar/extractMessage.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+$started = 0;
+foreach $line (<>) {
+	chomp $line;
+	if ($started == 0 and $line =~/message structure/) {
+		$started = 1;
+	}
+	elsif ($started == 1 and $line =~ /\w/) {
+		$line =~ s/\(//g;
+		$line =~ s/\)//g;
+		$line =~ s/\b[A-Z]+[0-9]+\b//g;
+		$line =~ s/\^//g;
+		$line =~ s/^ //;
+		if (not $line =~ /id 0/) {
+			print "$line\n";
+		}
+	}
+}
+
