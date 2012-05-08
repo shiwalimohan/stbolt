411bcb0b24ece9b5e345491095a31cf53b03c5af
Sam Wintermute <sam.wintermute@soartech.com>
2012-05-08 09:54:07 -0400

    multi-pass parsing

--- lgsoar/src/com/soartech/bolt/LGSupport.java
+++ lgsoar/src/com/soartech/bolt/LGSupport.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import net.sf.jlinkgrammar.Linkage;
 import net.sf.jlinkgrammar.Sentence;
 import sml.Agent;
+import sml.Agent.OutputEventInterface;
 import sml.Identifier;
+import sml.WMElement;
 
-public class LGSupport {
+public class LGSupport implements OutputEventInterface {
 	private static Agent agent;
 	public static String dictionaryPath = ""; 
 	private static Identifier lgInputRoot;
@@ -21,9 +23,21 @@ public class LGSupport {
 		if (agent != null) {
 			lgInputRoot = agent.CreateIdWME(agent.GetInputLink(), "lg");
 		}
+		agent.AddOutputHandler("preprocessed-sentence", this, null);
 	}
 	
 	public void handleSentence(String sentence) {
+		// load the sentence into WM
+		originalSentenceToWM(sentence);
+		
+		// Soar rules may modify it
+		
+		// wait for modified sentence on output
+	}
+	public void outputEventHandler(Object data, String agentName,
+			String attributeName, WMElement pWmeAdded) {
+		String sentence = preprocessedSentenceFromWM(pWmeAdded);
+		// call LG Parser
 		try {
 			net.sf.jlinkgrammar.parser.doIt(new String[]{sentence});
 		} catch (IOException e) {
@@ -112,4 +126,39 @@ public class LGSupport {
           
         }		
 	}
+
+	private void originalSentenceToWM(String sentence) {
+		Identifier root = agent.CreateIdWME(lgInputRoot, "original-sentence");
+        agent.CreateIntWME(root, "count", sentenceCount);
+        Identifier wordsWME = agent.CreateIdWME(root, "words");
+        sentence = sentence.replaceAll("(\\W)", " $1");
+        //System.out.println("padded: " + sentence);
+        String[] words = sentence.split("\\s+");
+        
+        for (int i=0; i<words.length; i++) {
+        	Identifier wordWME = agent.CreateIdWME(wordsWME, "word");
+        	agent.CreateStringWME(wordWME, "wvalue", words[i]);
+        	agent.CreateIntWME(wordWME, "wcount", i);
+        	//System.out.println("wd " + words[i]);
+        }
+
+	}
+	
+	private String preprocessedSentenceFromWM(WMElement pWmeAdded) {
+		String result = "";
+		
+		WMElement currentWME = pWmeAdded.ConvertToIdentifier().FindByAttribute("start", 0);
+		
+		while (currentWME != null) {
+			String word = currentWME.ConvertToIdentifier().GetParameterValue("word");
+			if (word != null) {
+				result += " " + word;
+			}
+			currentWME = currentWME.ConvertToIdentifier().FindByAttribute("next", 0);
+		}
+		
+		//System.out.println("got sentence: " + result);
+		return result;
+	}
+
 }
